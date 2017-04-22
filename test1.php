8fa4d2b2815d97658499ab75f535529dd1c80b0f
KuKuXiaoShuaiBin <3352593749@qq.com>
2017-04-22 16:19:36 +0800

    第二次提交代码

---
+++ test1.php
@@ -0,0 +1,26 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+/**
+ * Created by PhpStorm.
+ * User: 酷酷小帅斌
+ * Date: 2017/3/27
+ * Time: 18:21
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <script type="text/javascript">
+
+    </script>
+    <style type="text/css">
+
+    </style>
+</head>
+<body>
+
+</body>
+</html>
